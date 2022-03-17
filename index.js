37de98a10cecb04194b9ac03aa122502093bcd25
Pragadeeshwar <pragadeeshwar0801@gmail.com>
2022-03-17 14:04:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-const { run } = require("@probot/adapter-github-actions");
+const { run } = require('@probot/adapter-github-actions')
 const app = require("./app");
 
-run(app);
+const { createProbot } = require('probot');
+
+module.exports = (...handlers) => {
+  // Setup Probot app
+  const githubToken = process.env.GITHUB_TOKEN;
+  const probot = createProbot({ githubToken });
+  probot.setup(handlers);
+
+run(app).catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
