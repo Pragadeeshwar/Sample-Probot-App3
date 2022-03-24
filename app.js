9e635b33667bbe776913d31dc5a81a435c886d9c
Pragadeeshwar <pragadeeshwar0801@gmail.com>
2022-03-24 11:41:12 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { Probot, createProbot } = require("probot");
 const { createMyMiddleware } = require("my-probot-middleware")
-const app = require('./index');
+const app = require('./index.js');
 
 module.exports = createMyMiddleware(app, {
 	probot: createProbot(),
